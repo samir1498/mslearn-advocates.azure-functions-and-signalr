7b68fef8e81da84b247e5ea6d25799aad8b63e87
Samir <bettahar.samir@outlook.com>
2024-05-14 18:11:14 +0100

    updating connection string to cosmos db

--- start/server/src/functions/getStocks.ts
+++ start/server/src/functions/getStocks.ts
@@ -1,21 +1,21 @@
 import { app, input } from "@azure/functions";
 
 const cosmosInput = input.cosmosDB({
-    databaseName: 'stocksdb',
-    containerName: 'stocks',
-    connection: 'COSMOSDB_CONNECTION_STRING',
-    sqlQuery: 'SELECT * from c',
+  databaseName: "stocksdb",
+  containerName: "stocks",
+  connection: "COSMOSDB_CONNECTION_STRING",
+  sqlQuery: "SELECT * from c",
 });
 
-app.http('getStocks', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    extraInputs: [cosmosInput],
-    handler: (request, context) => {
-        const stocks = context.extraInputs.get(cosmosInput);
-        
-        return {
-            jsonBody: stocks,
-        };
-    },
+app.http("getStocks", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  extraInputs: [cosmosInput],
+  handler: (r, context) => {
+    const stocks = context.extraInputs.get(cosmosInput);
+
+    return {
+      jsonBody: stocks,
+    };
+  },
 });
--- start/server/src/functions/setPrice.ts
+++ start/server/src/functions/setPrice.ts
@@ -1,25 +1,34 @@
-import { app, InvocationContext, CosmosDBOutputOptions, Timer, output } from "@azure/functions";
+import {
+  app,
+  InvocationContext,
+  CosmosDBOutputOptions,
+  Timer,
+  output,
+} from "@azure/functions";
 import { updateData } from "../update";
-
+//comment
 const cosmosOutputConfig: CosmosDBOutputOptions = {
-    databaseName: 'stocksdb',
-    containerName: 'stocks',
-    connection: 'COSMOSDB_CONNECTION_STRING',
-    partitionKey: 'symbol'
+  databaseName: "stocksdb",
+  containerName: "stocks",
+  connection: "COSMOSDB_CONNECTION_STRING",
+  partitionKey: "symbol",
 };
 
 const cosmosOutput = output.cosmosDB(cosmosOutputConfig);
 
-export async function setPrice(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer function processed request.');
+export async function setPrice(
+  myTimer: Timer,
+  context: InvocationContext
+): Promise<void> {
+  context.log("Timer function processed request.");
 
-    const newStock = await updateData();
-    console.log(JSON.stringify(newStock));
-    context.extraOutputs.set(cosmosOutput, newStock);
+  const newStock = await updateData();
+  console.log(JSON.stringify(newStock));
+  context.extraOutputs.set(cosmosOutput, newStock);
 }
 
-app.timer('setPrice', {
-    schedule: '0 * * * * *',
-    extraOutputs: [cosmosOutput],
-    handler: setPrice
+app.timer("setPrice", {
+  schedule: "0 * * * * *",
+  extraOutputs: [cosmosOutput],
+  handler: setPrice,
 });
