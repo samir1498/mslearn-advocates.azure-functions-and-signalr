436e3be19705a013551014967fae41e77e11fe45
Samir <bettahar.samir@outlook.com>
2024-05-15 11:12:36 +0100

    Add BACKEND_URL environment variable

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
-// import { BACKEND_URL } from "./env";
-const BACKEND_URL = process.env.BACKEND_URL;
+import { BACKEND_URL } from "./env";
 
 const app = new Vue({
   el: "#app",
